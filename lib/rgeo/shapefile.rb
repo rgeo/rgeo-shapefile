38620a6acefdc2caa972b6f0b10cdf8bd6d9a9d8
Daniel Azuma <dazuma@gmail.com>
2010-12-06 14:20:28 -0800

    Moved files from rgeo core

---
+++ lib/rgeo/shapefile.rb
@@ -0,0 +1,62 @@
+# -----------------------------------------------------------------------------
+# 
+# Shapefile processing for RGeo
+# 
+# -----------------------------------------------------------------------------
+# Copyright 2010 Daniel Azuma
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder, nor the names of any other
+#   contributors to this software, may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+;
+
+
+# Dependencies
+require 'rgeo'
+
+
+# RGeo is a spatial data library for Ruby, provided by the "rgeo" gem.
+# 
+# The optional RGeo::Shapefile module provides a set of tools for reading
+# ESRI shapefiles.
+
+module RGeo
+  
+  
+  # This module contains an implementation of ESRI Shapefiles.
+  # Use the Shapefile::Reader class to read a shapefile, extracting
+  # geometry and attribute data from it.
+  # RGeo does not yet have support for writing shapefiles.
+  
+  module Shapefile
+  end
+  
+  
+end
+
+
+# Implementation files
+require 'rgeo/shapefile/reader'
