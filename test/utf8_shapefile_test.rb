aba6bde7156d8aee4152e4580eba76afdf1b312f
Ryan Wallace <rywall@gmail.com>
2019-02-21 16:31:17 -0500

    Add ability to specify the DBF encoding.
    Sometimes the DBF encoding cannot be detected automatically and needs to be specified manually.

---
+++ test/utf8_shapefile_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "rgeo/shapefile"
+
+class Utf8ShapefileTest < Minitest::Test
+  def test_attributes
+    _open_shapefile do |file_|
+      rec_ = file_.next
+      assert_equal("Tít Tribe", rec_["NAme"])
+    end
+  end
+
+  private
+
+  def _open_shapefile(&block_)
+    RGeo::Shapefile::Reader.open(
+      File.expand_path("utf8_shapefile/utf8",
+      File.dirname(__FILE__)),
+      dbf_encoding: "UTF-8",
+      &block_
+    )
+  end
+end
